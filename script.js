7421f20d8e6033430a7c4b9a46c75782ad2c22d0
somayeh-rzie <rezaie.somayeh79@gmail.com>
2021-12-11 22:33:00 +0330

    some changes in url

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
+//the website url that gets information for names
 var url = new URL('https://api.genderize.io/');
+
+
 
 //event listener for submit button; sends an http GET request
 //and recieves a JSON response
 
-function search(){
+function submitfunc(){
 
-  // localStorage.clear();
+  //prevent from submitting a form(prevents the default action of submit button)
+
+  document.getElementById("submit").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+
 
   var uname = document.getElementById("name").value;
 
   url.searchParams.set("name", uname);
 
   //create http connection
-
   var req = new XMLHttpRequest();
   req.overrideMimeType("application/json");
 
-  //sends http GET request
+  //send http GET request
   req.open('GET', url, true);
 
   //asynchronous
@@ -28,14 +35,7 @@ function search(){
     if(localStorage.getItem(uname)){
       document.getElementById("saved-data").textContent = "Saved Answer";
       document.getElementById("g").textContent = localStorage.getItem(uname);
-    // }else{
-      // document.getElementById("g").textContent = "no data saved before";
     }
-
-    // if (req.status != 200){
-    //   alert("Something Went Wrong! Probably Network Error");
-    //   return
-    // }
 
 
     //check response is empty or not
@@ -48,12 +48,12 @@ function search(){
     }
   };
 
-  //handle network error
+  //network error handling
   try{
     req.send(null);
   } catch(exception){
     if(exception.name == 'NetworkError'){
-      alert('There was a network error.');
+      alert('We recognized a network error!');
    }
   }
 }
@@ -63,9 +63,11 @@ function search(){
 
 //Save button event listener
 
-function savedata(){
+function savefunc(){
 
   var uname = document.getElementById("name").value;
+
+  //checks which radio button is selected
 
   if(document.getElementById("male").checked == true) {   
 
@@ -75,19 +77,19 @@ function savedata(){
 
     localStorage.setItem(uname, "female");
   }
-  alert("gender saved");
+  // alert("gender successfully saved");
 }
 
 
 
 //Clears the saved information from local storage for a specific name
 
-function cleardata(){
+function clearfunc(){
 
   var uname = document.getElementById("name").value;
 
   localStorage.removeItem(uname);
 
-  alert("gender cleared");
+  // alert("gender successfully cleared");
 
 }
